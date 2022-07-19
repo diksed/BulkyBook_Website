eeb943556f9c037ae97910d0bd6791735c43e517
diksed <diksed00@gmail.com>
2022-07-19 17:49:16 +0300

    Category View created

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -16,5 +16,9 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
